9136b612c927f89a80e6ec29f222bab3b2131b68
Aleksandr Kulikov <aleksandr-kulikov-13@mail.ru>
2023-06-08 23:57:16 +0300

    feat: добавлен app и папка routes

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log(12234444);
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const { PORT = 300 } = process.env;
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`Слушаю порт ${PORT}`);
+});
