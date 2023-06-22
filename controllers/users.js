9beb70db19987c82ff30ec0bbe23ced7faa1f186
Aleksandr Kulikov <aleksandr-kulikov-13@mail.ru>
2023-06-22 15:37:46 +0300

    feat: добавляет auth изменяет все контроллеры

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,42 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bcrypt = require('bcrypt');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const jsonwebtoken = require('jsonwebtoken');
 const User = require('../models/user');
+const ErrorCode = require('../utils/errors/ErrorCode');
+const ConflictRequest = require('../utils/errors/ConflictRequest');
+const NotFoundError = require('../utils/errors/NotFoundError');
 const {
-  ERROR_CODE, ERROR_NO_USER, ERROR_SERVER, SUCCESS,
+  ERROR_CODE, ERROR_NO_USER, SUCCESS, BASE_ERROR,
 } = require('../utils/errors/constants');
 
-const getUsers = (req, res) => User.find({})
-  .then((users) => res.status(SUCCESS).send({ data: users }))
-  .catch(() => res.status(ERROR_SERVER).send('Ошибка сервера'));
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email }).select('+password')
+    .orFail(() => next(new NotFoundError('Пользователь не найден')))
+    .then((user) => bcrypt.compare(password, user.password).then((matched) => {
+      if (matched) {
+        return user;
+      }
+      return next(new NotFoundError('Пользователь не найден'));
+    }))
+    .then((user) => {
+      const jwt = jsonwebtoken.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      res.send({ jwt });
+    })
+    .catch(next);
+};
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => res.status(SUCCESS).send({ data: user }))
+    .catch(next);
+};
 
-const getUsersId = (req, res) => User.findById(req.params.userId)
+const getUsers = (req, res, next) => User.find({})
+  .then((users) => res.status(SUCCESS).send({ data: users }))
+  .catch(next);
+
+const getUsersId = (req, res, next) => User.findById(req.params.userId)
   .then((user) => {
     if (user) {
       res.send({ data: user });
@@ -19,28 +48,39 @@ const getUsersId = (req, res) => User.findById(req.params.userId)
     if (error.name === 'CastError') {
       res.status(ERROR_CODE).send({ message: 'Некорректный _id пользователя' });
     } else {
-      res.status(ERROR_SERVER).send('Ошибка сервера');
+      next(error);
     }
   });
 
-const createUsers = (req, res) => {
-  const { name, about, avatar } = req.body;
-  return User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+const createUsers = (req, res, next) => {
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  // return User.create({ name, about, avatar })
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res.status(SUCCESS).send({
+      email: user.email,
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+    }))
+    // .then((user) => res.send({ data: user }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
-        res
-          .status(ERROR_CODE)
-          .send({
-            message: 'Переданы некорректные данные при создании пользователя',
-          });
+        next(new ErrorCode('Переданы некорректные данные при создании пользователя'));
+      } else if (error.code === BASE_ERROR) {
+        next(new ConflictRequest('Пользователь с указанной почтой уже есть в системе'));
+        // res.status(ERROR_SERVER).send('Ошибка сервера');
       } else {
-        res.status(ERROR_SERVER).send('Ошибка сервера');
+        next(error);
       }
     });
 };
 
-const changeUserInfo = (req, res) => {
+const changeUserInfo = (req, res, next) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
@@ -53,18 +93,14 @@ const changeUserInfo = (req, res) => {
     .then((userInfo) => res.send({ data: userInfo }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
-        res
-          .status(ERROR_CODE)
-          .send({
-            message: 'Переданы некорректные данные при обновлении профиля',
-          });
+        next(new ErrorCode('Переданы некорректные данные при обновлении профиля'));
       } else {
-        res.status(ERROR_SERVER).send('Ошибка сервера');
+        next(error);
       }
     });
 };
 
-const changeAvatar = (req, res) => {
+const changeAvatar = (req, res, next) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
@@ -77,18 +113,16 @@ const changeAvatar = (req, res) => {
     .then((userAvatar) => res.send({ data: userAvatar }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
-        res
-          .status(ERROR_CODE)
-          .send({
-            message: 'Переданы некорректные данные при обновлении аватара',
-          });
+        next(new ErrorCode('Переданы некорректные данные при обновлении аватара'));
       } else {
-        res.status(ERROR_SERVER).send('Ошибка сервера');
+        next(error);
       }
     });
 };
 
 module.exports = {
+  login,
+  getCurrentUser,
   getUsers,
   getUsersId,
   createUsers,
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,36 @@
 const router = require('express').Router();
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers,
   getUsersId,
-  createUsers,
   changeUserInfo,
   changeAvatar,
+  getCurrentUser,
 } = require('../controllers/users');
 
+router.get('/me', getCurrentUser);
 router.get('/', getUsers);
-router.get('/:userId', getUsersId);
-router.post('/', createUsers);
-router.patch('/me', changeUserInfo);
-router.patch('/me/avatar', changeAvatar);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24)
+      .required(),
+  }),
+}), getUsersId);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), changeUserInfo);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().min(4).pattern(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/).required(),
+    // https://habr.com/ru/post/66931/
+  }),
+}), changeAvatar);
 
 module.exports = router;
