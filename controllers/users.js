7be66c28698069dc153831dff88ef52dee3b3898
Aleksandr Kulikov <aleksandr-kulikov-13@mail.ru>
2023-06-23 14:24:10 +0300

    fix: 6 test

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,20 @@
-// eslint-disable-next-line import/no-unresolved
-const bcrypt = require('bcryptjs');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bcrypt = require('bcrypt');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const jsonwebtoken = require('jsonwebtoken');
 const User = require('../models/user');
+// const ErrorCode = require('../utils/errors/ErrorCode');
+// const ConflictRequest = require('../utils/errors/ConflictRequest');
 const NotFoundError = require('../utils/errors/NotFoundError');
-const BadRequestError = require('../utils/errors/BadRequestError');
 const NotUsersFound = require('../utils/errors/NotUsersFound');
 const ConflictRequest = require('../utils/errors/ConflictRequest');
+const BadRequestError = require('../utils/errors/BadRequestError');
 const { SUCCESS, BASE_ERROR } = require('../utils/errors/constants');
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
-  User
-    .findOne({ email }).select('+password')
-    .orFail(new NotUsersFound('Пользователь не найден'))
+  User.findOne({ email }).select('+password')
+    .orFail(() => next(new NotUsersFound('Пользователь не найден')))
     .then((user) => bcrypt.compare(password, user.password).then((matched) => {
       if (matched) {
         return user;
@@ -25,7 +27,6 @@ const login = (req, res, next) => {
     })
     .catch(next);
 };
-
 const getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => res.status(SUCCESS).send({ data: user }))
@@ -56,6 +57,7 @@ const createUsers = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
+  // return User.create({ name, about, avatar })
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
@@ -66,11 +68,13 @@ const createUsers = (req, res, next) => {
       about: user.about,
       avatar: user.avatar,
     }))
+    // .then((user) => res.send({ data: user }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
         next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
       } else if (error.code === BASE_ERROR) {
         next(new ConflictRequest('Пользователь с указанной почтой уже есть в системе'));
+        // res.status(ERROR_SERVER).send('Ошибка сервера');
       } else {
         next(error);
       }
@@ -79,10 +83,14 @@ const createUsers = (req, res, next) => {
 
 const changeUserInfo = (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, {
-    new: true,
-    runValidators: true,
-  })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
     .then((userInfo) => res.send({ data: userInfo }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
@@ -95,10 +103,14 @@ const changeUserInfo = (req, res, next) => {
 
 const changeAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, {
-    new: true,
-    runValidators: true,
-  })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
     .then((userAvatar) => res.send({ data: userAvatar }))
     .catch((error) => {
       if (error.name === 'ValidationError' || error.name === 'CastError') {
@@ -110,11 +122,11 @@ const changeAvatar = (req, res, next) => {
 };
 
 module.exports = {
+  login,
+  getCurrentUser,
   getUsers,
   getUsersId,
   createUsers,
   changeUserInfo,
   changeAvatar,
-  login,
-  getCurrentUser,
 };
